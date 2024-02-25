41411a09722bc67a9ce593f0d14ba062bf0f953b
youjungHwang <dbwjd5032@gmail.com>
2024-02-26 01:32:27 +0900

    feat: 부하테스트 코드 작성

---
+++ order_test/create_product.py
@@ -0,0 +1,87 @@
+import pymysql
+from datetime import datetime
+
+def insert_products():
+    try:
+        # 데이터베이스 연결 설정
+        connection = pymysql.connect(
+            host='localhost',
+            port=13310,
+            database='product_database',
+            user='youjung',
+            password='timegate2249207'
+        )
+        cursor = connection.cursor()
+
+        # 일반 상품 등록
+        normal_product_query = """
+        INSERT INTO product (product_name, price, product_type, available_from, available_until) 
+        VALUES (%s, %s, %s, %s, %s)
+        """
+        normal_product_values = ("일반 상품", 1000, "REGULAR", None, None)
+        cursor.execute(normal_product_query, normal_product_values)
+        normal_product_id = cursor.lastrowid
+
+        # 예약 상품 등록
+        pre_order_product_query = """
+        INSERT INTO product (product_name, price, product_type, available_from, available_until) 
+        VALUES (%s, %s, %s, %s, %s)
+        """
+        available_at = datetime(2024, 2, 22, 7, 0, 0)
+        end_at = datetime(2025, 2, 22, 7, 0, 0)
+        pre_order_product_values = ('예약 상품', 500, 'RESERVED', available_at, end_at)
+        cursor.execute(pre_order_product_query, pre_order_product_values)
+        pre_order_product_id = cursor.lastrowid
+
+        connection.commit()
+
+        print(f"일반 상품과 예약 상품이 성공적으로 등록되었습니다. 상품 번호: {normal_product_id}, {pre_order_product_id}")
+
+    except pymysql.MySQLError as e:
+        print("데이터베이스 연결 또는 쿼리 실행 중 오류 발생:", e)
+    finally:
+        # 연결 종료
+        cursor.close()
+        connection.close()
+        print("MySQL 연결이 종료됨.")
+    return [normal_product_id, pre_order_product_id]
+
+def insert_stocks(firstProductId, secondProductId):
+    try:
+        # 데이터베이스 연결 설정
+        connection = pymysql.connect(
+            host='localhost',
+            port=13313,
+            database='stock_database',
+            user='youjung',
+            password='timegate2249207'
+        )
+        cursor = connection.cursor()
+
+        # 재고 등록
+        stock_query = "INSERT INTO stock (product_id, stock) VALUES (%s, %s)"
+        cursor.execute(stock_query, (firstProductId, 10))
+
+        retrieve_query = "SELECT * FROM stock WHERE product_id = %s"
+        cursor.execute(retrieve_query, (firstProductId,))
+        inserted_data = cursor.fetchone()
+        print(f"삽입된 데이터: {inserted_data}")
+
+        cursor.execute(stock_query, (secondProductId, 10))
+        cursor.execute(retrieve_query, (secondProductId,))
+        inserted_data = cursor.fetchone()
+        print(f"삽입된 데이터: {inserted_data}")
+
+        connection.commit()
+
+    except pymysql.MySQLError as e:
+        print("데이터베이스 연결 또는 쿼리 실행 중 오류 발생:", e)
+    finally:
+        # 연결 종료
+        cursor.close()
+        connection.close()
+        print("MySQL 연결이 종료됨.")
+
+if __name__ == "__main__":
+    insert_product_result = insert_products()
+    insert_stocks(insert_product_result[0], insert_product_result[1])
---
+++ order_test/http_request_tool.py
@@ -0,0 +1,79 @@
+import requests
+from concurrent.futures import ThreadPoolExecutor
+from datetime import datetime
+
+def send_http_request(buyerNum):
+    order_url = "http://localhost:8083/orders-service/api/v1/orders"
+    payment_url = "http://localhost:8084/payment-service/api/v1/payments"
+
+    try:
+
+    #     # {
+    # "userId" : 1,
+    # "productId" : 2,
+    # "price" : "10000",
+    # "quantity" : 1
+    # }
+
+        # POST 요청을 보냄(!!!!!!!!!!!!!!아이템 번호 바꿔야함)
+        response = requests.post(order_url, json={"userId": buyerNum, "productId": 1, "price": 1000, "quantity":1})
+        # print(f"Request to {order_url} with buyerNum {buyerNum} completed with status code {response.status_code}")
+
+        # 응답을 JSON 형식으로 파싱
+        data = response.json()
+
+#     {
+#     "status": "CREATED",
+#     "message": "일반 상품 주문 생성 성공",
+#     "data": {
+#         "orderId": 10,
+#         "userId": 1,
+#         "productId": 3,
+#         "price": 44400.00,
+#         "quantity": 1,
+#         "ordersType": "IN_PROGRESS"
+#     }
+#   }
+
+        # "orderId" 키가 있는지 확인
+        if "orderId" in data.get("data", {}):
+            orderId = data["data"]["orderId"]
+            print(f"Received orderId: {orderId}")
+
+            # 결제 요청을 보냄
+            payment_response = requests.post(payment_url, json={"orderId": orderId})
+
+            if payment_response.status_code == 201:
+                payment_data = payment_response.json()
+                print(f"Payment for orderId {orderId} completed successfully, {payment_data['data']}")
+            else:
+                payment_data = payment_response.json()
+                print(f"Payment for orderId {orderId} failed with status code {payment_data}")
+        # else:
+        #     print("Response does not contain 'orderNum' key")
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error sending request to {order_url}: {e}")
+
+def main():
+    # Set the number of concurrent requests (N)
+    num_requests = 100  # buyerNum을 1부터 10000까지 보낼 것이므로 요청 수를 10000으로 설정
+
+    start_time = datetime.now()  # 코드 실행 시작 시간
+
+    # Create a ThreadPoolExecutor to send concurrent requests
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        # Use a list comprehension to create a list of tasks
+        tasks = [executor.submit(send_http_request, buyerNum) for buyerNum in range(1, num_requests + 1)]
+
+        # Wait for all tasks to complete
+        for future in tasks:
+            future.result()
+
+    end_time = datetime.now()  # 코드 실행 종료 시간
+    duration = end_time - start_time
+
+    print(f"실행 소요시간 {duration}")
+
+if __name__ == "__main__":
+    main()
